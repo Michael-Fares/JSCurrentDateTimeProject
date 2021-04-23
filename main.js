413c27e98f1a138c16f7abc541fe4236582fb6ed
Michael-Fares <mjfares@gmail.com>
2021-04-23 08:45:13 -0500

    time and date repo init

--- main.js
+++ main.js
@@ -15,36 +15,83 @@ const displayDate = () => {
  
 
 // Write a JavaScript program to convert a number to a string.
-
-
-
-// Write a JavaScript program to convert a string to the number.
-
-
-
-// Write a JavaScript program that takes in different datatypes and prints out whether they are a:
-  // * Boolean
-  // * Null
-  // * Undefined
-  // * Number
-  // * NaN
-  // * String
+//I had to remove the parameter num and instead accese the value of the input to get it to work
+const numberToString = () => {
+  const num = document.getElementById('number').value
+  const s = num.toString();
+  console.log(s, typeof s);
+  document.getElementById("display-element").innerHTML = s;
+  return s;
+  }
   
-
   
-// Write a JavaScript program that adds 2 numbers together.
-
-
-
-// Write a JavaScript program that runs only when 2 things are true.
-
-
-
-// Write a JavaScript program that runs when 1 of 2 things are true.
-
-
-
-// Write a JavaScript program that runs when both things are not true.  
+  
+  
+  // Write a JavaScript program to convert a string to the number.
+  //does not parse ints in middle of string
+  const stringToNumber = () => {
+  const str = document.getElementById('string').value
+  const num = parseInt(str);
+  console.log(num, typeof num);
+  document.getElementById("display-element").innerHTML = num;
+  return num;
+  }
+  
+  
+  
+  // Write a JavaScript program that takes in different datatypes and prints out whether they are a:
+    // * Boolean
+    // * Null
+    // * Undefined
+    // * Number
+    // * NaN
+    // * String
+  
+    const checkDataType = () => {
+    const value = document.getElementById('data').value
+    if(value === "") {
+    document.getElementById("display-element").innerHTML = "null";
+    return "null";
+    }
+else if(value === "true" || value === "false") {
+  document.getElementById("display-element").innerHTML = "boolean";
+    return "boolean";
+}
+else if(!Number.isNaN(Number(value))) {
+  document.getElementById("display-element").innerHTML = "number";  
+  return "number";
+}
+else {
+  document.getElementById("display-element").innerHTML = "string";
+    return "string";
+    }
+  }
+  
+  
+   
+  // Need to figure out how to get DOM to take in the store input value in the actual arguments.
+    // Write a JavaScript program that adds 2 numbers together.
+    const addTwoNumbers = (x,y) => {
+     return x + y;
+    }
+  
+  // Write a JavaScript program that runs when 1 of 2 things are true. - check if someone is either 20 or younger or 40 or older
+  
+  const checkValue = (x) => {
+    if (x <=20 || x >= 40) {
+      return (true, 'The number is either less than or equal to 20, or greater than or equal to 40');
+    } else {
+      return (false, 'The number is between 20 and 40');
+    }
+  }
+  
+  // // Write a JavaScript program that runs when both things are not true. - Check that the age range is 20-40 inclusive. 
+  // Why can't I get this one to work?
+  const checkNumbers = (x,y) => {
+   if (!x===10 && !y===10) {
+     return 'neither # is 10';
+   }
+  }
 
 // ***************************
 //         PART TWO
